a496d4e45b4d0711eae12ba27125d2e59c20be34
Alejandro0102 <alexdltc21@gmail.com>
2023-05-01 18:07:07 +0200

    He realizado la clase Password2

--- src/PracticaGit/Password2.java
+++ src/PracticaGit/Password2.java
@@ -1,5 +1,37 @@
 package PracticaGit;
 
-public class Password2 {
+import java.util.Random;
 
+public class Password2 {
+private static String generarPassword(int longitud, String contrasena) {
+		
+		// GENERAR CONTRASEÑA DE LONGITUD ESPECIFICADA
+		    for (int i = 0; i < longitud; i++) {
+		        if (Math.random() < 0.5) {
+		// GENERAR UN NÚMERO ALEATORIO ENTRE 0 Y 9
+		            int numero = (int) (Math.random() * 10);
+		            contrasena += Integer.toString(numero);
+		        } else {
+		// GENERAR UNA LETRA MINÚSCULA ALEATORIA
+		            char letra = (char) ((int) (Math.random() * 26) + 'a');
+		            contrasena += letra;
+		        }
+		    }
+		
+			System.out.println(contrasena);
+		 // DEVOLVER LA CONTRASEÑA GENERADA
+		return contrasena;
+		}
+		
+			    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz._*ñÑ";
+			    public static String passwordValida(int longitud, String contrasena) {
+			        Random rand = new Random();
+			        StringBuilder password = new StringBuilder();
+			        for (int i = 0; i < longitud; i++) {
+			            char caracter = CHARACTERS.charAt(rand.nextInt(CHARACTERS.length()));
+			            password.append(caracter);
+			        }
+			        System.out.println(contrasena);
+			        return password.toString();
+			    }
 }
